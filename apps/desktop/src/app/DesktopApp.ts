4afd30865f50faf04eeca5f9f0b45c85b4f88c81
sheehanmunim <sheehanmunim@gmail.com>
2026-09-16 12:36:28 -0400

    take(#10163): feat(desktop): support system tray and persist on close on Windows
    Squashed cherry-pick of upstream PR pingdotgg/t3code#10163 (7 commits:
    ed597dfdd6, f9740a8654, 8392939236, 2b0df2cb00, 3187bc1981, 2bebe5a60e,
    e8be38af69). Tray tooltip/menu labels come from environment.displayName
    (fork branding) and the icon from DesktopAssets.iconPaths (staged MT icons).

--- apps/desktop/src/app/DesktopApp.ts
+++ apps/desktop/src/app/DesktopApp.ts
@@ -32,6 +32,7 @@ import * as DesktopAppSettings from "../settings/DesktopAppSettings.ts";
 import * as DesktopShellEnvironment from "../shell/DesktopShellEnvironment.ts";
 import * as DesktopState from "./DesktopState.ts";
 import * as DesktopRemoteUpdates from "../updates/DesktopRemoteUpdates.ts";
+import * as DesktopTray from "./DesktopTray.ts";
 import * as DesktopUpdates from "../updates/DesktopUpdates.ts";
 import * as DesktopSnapShot from "../snapShot/DesktopSnapShot.ts";
 import * as DesktopWslBackend from "../wsl/DesktopWslBackend.ts";
@@ -298,6 +299,7 @@ const startup = Effect.gen(function* () {
   const desktopSettings = yield* DesktopAppSettings.DesktopAppSettings;
   const preReadyElectronOptions = yield* DesktopPreReadyPlatform.DesktopPreReadyElectronOptions;
   const safeStorage = yield* ElectronSafeStorage.ElectronSafeStorage;
+  const tray = yield* DesktopTray.DesktopTray;
   const updates = yield* DesktopUpdates.DesktopUpdates;
   const environment = yield* DesktopEnvironment.DesktopEnvironment;
 
@@ -357,6 +359,7 @@ const startup = Effect.gen(function* () {
   }
   yield* appIdentity.configure;
   yield* applicationMenu.configure;
+  yield* tray.configure;
   yield* updates.configure;
   yield* DesktopRemoteUpdates.listen;
   yield* linuxUrlHandler.register;
